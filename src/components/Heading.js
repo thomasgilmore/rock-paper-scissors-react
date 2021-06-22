d8de97ddd01db63121eee5444e823c38793d9f24
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-22 12:00:56 -0400

    Heading.js

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -6,7 +6,10 @@ export default function Heading(props) {
         <div className="headingDiv">
             <div className="card">
                 <img src={props.img} className="logo" />
-                <h1 className="score" >{props.score}</h1>
+                <div className="scoreCard">
+                    <p className="scoreText">SCORE</p>
+                    <h1 className="score">{props.score}</h1>
+                </div>
             </div>
         </div>
     )
