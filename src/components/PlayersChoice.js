f45b0bb18e7306c13a6c9f0a358b0dc74b95f38e
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-23 17:47:43 -0400

    buttons style

--- src/components/PlayersChoice.js
+++ src/components/PlayersChoice.js
@@ -4,11 +4,15 @@ import backgroundTriangle from '../images/bg-triangle.svg';
 
 export default function PlayersChoice(props) {
     return (
+        <div>
+        <div className="backgroundTriangle">
+        <img src={backgroundTriangle} className="backgroundTriangle" alt="backgroundTriangle" />
+        </div>
         <div className="playersChoiceDiv">
-            <img src={backgroundTriangle} className="backgroundTriangle" alt="backgroundTriangle" />
-            <div className="rockButtonBorder"><button className="rockButton"></button></div>
             <div className="paperButtonBorder"><button className="paperButton"></button></div>
+            <div className="rockButtonBorder"><button className="rockButton"></button></div>
             <div className="scissorButtonBorder"><button className="scissorButton"></button></div>
+        </div>
         </div>
     )
 }
