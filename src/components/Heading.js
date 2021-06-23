03f9c1bf9d147c0546e97ab264203ee9dde62392
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-22 21:46:31 -0400

    alt for image

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -5,7 +5,7 @@ export default function Heading(props) {
     return (
         <div className="headingDiv">
             <div className="card">
-                <img src={props.img} className="logo" />
+                <img src={props.img} className="logo" alt="logo" />
                 <div className="scoreCard">
                     <p className="scoreText">SCORE</p>
                     <h1 className="score">{props.score}</h1>
