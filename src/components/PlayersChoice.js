e4754c511b0d9134f1d1090bcf415d6469322c68
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-23 17:19:45 -0400

    center allign images

--- src/components/PlayersChoice.js
+++ src/components/PlayersChoice.js
@@ -4,7 +4,7 @@ import backgroundTriangle from '../images/bg-triangle.svg';
 
 export default function PlayersChoice(props) {
     return (
-        <div>
+        <div className="playersChoiceDiv">
             <img src={backgroundTriangle} className="backgroundTriangle" alt="backgroundTriangle" />
             <div className="rockButtonBorder"><button className="rockButton"></button></div>
             <div className="paperButtonBorder"><button className="paperButton"></button></div>
