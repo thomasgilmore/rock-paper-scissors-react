88a439e4c85440ad430a62d737bfd158fe6497c3
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-23 21:48:54 -0400

    rules button function and close button function

--- src/components/RockPaperScissors.js
+++ src/components/RockPaperScissors.js
@@ -4,13 +4,25 @@ import logo from '../images/logo.svg';
 import './rockpaperscissors.css';
 import PlayersChoice from './PlayersChoice';
 import RulesButton from './RulesButton';
+import Rules from './Rules';
 
 export default class RockPaperScissors extends Component {
     constructor(props) {
         super(props);
         this.state = {
             score: 0,
+            rulesOpen: false
         };
+        this.rulesButtonClick = this.rulesButtonClick.bind(this);
+        this.closeButtonClick = this.closeButtonClick.bind(this);
+    }
+
+    rulesButtonClick() {
+        this.state.rulesOpen ? this.setState({ rulesOpen: false }) : this.setState({ rulesOpen: true });
+    }
+
+    closeButtonClick() {
+        this.setState({ rulesOpen: false });
     }
 
     render() {
@@ -18,7 +30,8 @@ export default class RockPaperScissors extends Component {
             <div>
                 <Heading img={logo} score={this.state.score} />
                 <PlayersChoice />
-                <RulesButton />
+                <RulesButton rulesButtonClick={this.rulesButtonClick} />
+                {this.state.rulesOpen ? <Rules closeButtonClick={this.closeButtonClick} /> : null}
             </div>
         )
     }
