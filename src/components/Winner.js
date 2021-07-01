ab81b5f13d10c44f3ffdd729277eebf3cf5e95ae
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-07-01 15:52:42 -0400

    working on Winner.js

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -8,6 +8,10 @@ export default function Winner(props) {
                 <h2 className="youPickedTitle">YOU PICKED</h2>
                 <div className={props.playersChoice === "rock" ? "rockButtonBorderWinner" : props.playersChoice === "paper" ? "paperButtonBorderWinner" : "scissorButtonBorderWinner"} onClick={null}><button className={props.playersChoice === "rock" ? "rockButtonWinner" : props.playersChoice === "paper" ? "paperButtonWinner" : "scissorButtonWinner"}></button></div>
             </div>
+            <div className="winnerIsDiv">
+                <h1 className="winnerIsTitle">{props.playerIsWinner === true ? "YOU WIN" : props.computerIsWinner === true ? "YOU LOSE" : "TIE"}</h1>
+                <button className="playAgainButton" onClick={props.playAgain}>PLAY AGAIN</button>
+            </div>
             <div className="theHousePickedDiv">
                 <h2 className="theHousePickedTitle">THE HOUSE PICKED</h2>
                 <div className={props.computersChoice === "rock" ? "rockButtonBorderWinner" : props.computersChoice === "paper" ? "paperButtonBorderWinner" : "scissorButtonBorderWinner"} onClick={null}><button className={props.computersChoice === "rock" ? "rockButtonWinner" : props.computersChoice === "paper" ? "paperButtonWinner" : "scissorButtonWinner"}></button></div>
