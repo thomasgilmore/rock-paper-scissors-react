f40fd56fc74710f7f1b880ae4bed7d2b0cc7f9f6
Thomas Gilmore <thomasgilmore95@gmail.com>
2021-06-30 21:38:06 -0400

    created functions for rock paper scissors game

--- src/components/RockPaperScissors.js
+++ src/components/RockPaperScissors.js
@@ -5,16 +5,28 @@ import './rockpaperscissors.css';
 import PlayersChoice from './PlayersChoice';
 import RulesButton from './RulesButton';
 import Rules from './Rules';
+import Winner from './Winner';
 
 export default class RockPaperScissors extends Component {
     constructor(props) {
         super(props);
         this.state = {
             score: 0,
-            rulesOpen: false
+            rulesOpen: false,
+            playersChoice: '',
+            playerChose: false,
+            computersChoice: '',
+            isTie: false,
+            playerIsWinner: false,
+            computerIsWinner: false
         };
         this.rulesButtonClick = this.rulesButtonClick.bind(this);
         this.closeButtonClick = this.closeButtonClick.bind(this);
+        this.computersChoice = this.computersChoice.bind(this);
+        this.winner = this.winner.bind(this);
+        this.rockButtonChoiceClick = this.rockButtonChoiceClick.bind(this);
+        this.paperButtonChoiceClick = this.paperButtonChoiceClick.bind(this);
+        this.scissorsButtonChoiceClick = this.scissorsButtonChoiceClick.bind(this);
     }
 
     rulesButtonClick() {
@@ -25,11 +37,90 @@ export default class RockPaperScissors extends Component {
         this.setState({ rulesOpen: false });
     }
 
+    computersChoice() {
+        let randomNumber = Math.floor(Math.random() * 3) + 1;
+        if (randomNumber === 1) {
+            return 'rock';
+        } else if (randomNumber === 2) {
+            return 'paper';
+        } else {
+            return 'scissors';
+        }
+    }
+
+    winner(playersChoice, computersChoice) {
+        let score = this.state.score;
+        let isTie = this.state.isTie;
+        let playerIsWinner = this.state.playerIsWinner;
+        let computerIsWinner = this.state.computerIsWinner;
+        if (playersChoice === "rock") {
+            if (computersChoice === "rock") {
+                isTie = true;
+            } else if (computersChoice === "paper") {
+                score -= 1;
+                computerIsWinner = true;
+            }  else if (computersChoice === "scissors") {
+                score += 1;
+                playerIsWinner = true;
+            }
+        } else if (playersChoice === "paper") {
+            if (computersChoice === "rock") {
+                score += 1;
+                playerIsWinner = true;
+            } else if (computersChoice === "paper") {
+                isTie = true;
+            }  else if (computersChoice === "scissors") {
+                score -= 1;
+                computerIsWinner = true;
+            }
+        } else if (playersChoice === "scissors") {
+            if (computersChoice === "rock") {
+                score -= 1;
+                computerIsWinner = true;
+            } else if (computersChoice === "paper") {
+                score += 1;
+                playerIsWinner = true;
+            }  else if (computersChoice === "scissors") {
+                isTie = true;
+            }
+        }
+        this.setState({ score, isTie, playerIsWinner, computerIsWinner });
+    }
+
+    rockButtonChoiceClick() {
+        let playersChoice = 'rock';
+        let computersChoice = this.computersChoice();
+        this.winner(playersChoice, computersChoice);
+        this.setState({ playersChoice, playerChose: true, computersChoice });
+    }
+
+    paperButtonChoiceClick() {
+        let playersChoice = 'paper';
+        let computersChoice = this.computersChoice();
+        this.winner(playersChoice, computersChoice);
+        this.setState({ playersChoice, playerChose: true, computersChoice });
+    }
+
+    scissorsButtonChoiceClick() {
+        let playersChoice = 'scissors';
+        let computersChoice = this.computersChoice();
+        this.winner(playersChoice, computersChoice);
+        this.setState({ playersChoice, playerChose: true, computersChoice});
+    }
+
     render() {
         return (
             <div>
                 <Heading img={logo} score={this.state.score} />
-                <PlayersChoice />
+                {this.state.playerChose ? <Winner playersChoice={this.state.playersChoice}
+                    computersChoice={this.state.computersChoice} 
+                    isTie={this.state.isTie}
+                    playerIsWinner={this.state.playerIsWinner}
+                    computerIsWinner={this.state.computerIsWinner} /> : 
+                    <PlayersChoice rockButtonChoiceClick={this.rockButtonChoiceClick}
+                    paperButtonChoiceClick={this.paperButtonChoiceClick}
+                    scissorsButtonChoiceClick={this.scissorsButtonChoiceClick} />
+                }
                 <RulesButton rulesButtonClick={this.rulesButtonClick} />
                 {this.state.rulesOpen ? <Rules closeButtonClick={this.closeButtonClick} /> : null}
             </div>
